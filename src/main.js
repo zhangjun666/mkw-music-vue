412e688d2f369ac4d2f2dd8ada1b55774dea9901
zhangjun666 <7489320+zhangjun7777@user.noreply.gitee.com>
2021-05-31 10:57:42 +0800

    chore: 项目配置 tab组件

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// 引入全局样式
-import '@assets/scss/index.scss'
+// 引入全局样式文件
+import '@/assets/scss/index.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+  .use(store)
+  .use(router)
+  .mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-const registerRouter = require('./backend/router')
 
 module.exports = {
   css: {
@@ -11,18 +10,5 @@ module.exports = {
         `
       }
     }
-  },
-  devServer: {
-    before(app) {
-      registerRouter(app)
-    }
-  },
-  configureWebpack: (config) => {
-    if (process.env.npm_config_report) {
-      const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-      config.plugins.push(new BundleAnalyzerPlugin())
-    }
-  },
-  productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/music-next/' : '/'
+  }
 }
